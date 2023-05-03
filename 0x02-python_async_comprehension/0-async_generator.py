2f9485e11aa8d06e9af293c01bfdba309f54e0b5
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-05-03 18:09:41 +0300

    async

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-""" Task 1.
+""" Task 0.
 """
 
 import asyncio
@@ -13,4 +13,3 @@ async def async_generator() -> Generator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.random() * 10
-        
